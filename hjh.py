e2cb0bec8dfcdd18ac7b960f1027f4d98cb891ba
hjh <38830650+hjhroyal@users.noreply.github.com>
2022-03-06 10:38:49 +0800

    Add files via upload

--- hjh.py
+++ hjh.py
@@ -6,24 +6,23 @@ from scipy.linalg import norm
 from sklearn.preprocessing import scale, LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
 
-def readData(sc, tg):
-    data = sio.loadmat('Datasets/OC10_vgg6/' + sc + '.mat')  # source domain
-    Xs, ys = data['FTS'].astype(np.float64), data['LABELS'].ravel()
-    ys = LabelEncoder().fit(ys).transform(ys).astype(np.int)
+def readData(test_size):
+    domains = ['amazon', 'caltech', 'dslr', 'webcam']
+    X_all = np.empty((0, 4096))
+    y_all = np.empty((0),dtype=np.int)
+    for domain in domains:
+        data = sio.loadmat('OC10_vgg6/' + domain + '.mat')  # source domain
+        Xs, ys = data['FTS'].astype(np.float64), data['LABELS'].ravel()
+        ys = LabelEncoder().fit(ys).transform(ys).astype(np.int)
+        Xs = Xs / norm(Xs, axis=1, keepdims=True)
+        X_all = np.concatenate((X_all,Xs))
+        y_all = np.concatenate((y_all,ys))
+    return train_test_split(X_all, y_all, test_size=test_size)
 
-    data = sio.loadmat('Datasets/OC10_vgg6/' + tg + '.mat')  # target domain
-    Xt, yt = data['FTS'].astype(np.float64), data['LABELS'].ravel()
-    yt = LabelEncoder().fit(yt).transform(yt).astype(np.int)
-
-    Xs = Xs / norm(Xs, axis=1, keepdims=True)
-    Xt = Xt / norm(Xt, axis=1, keepdims=True)
-    return np.array(Xs), np.array(ys), np.array(Xt), np.array(yt)
-domains = ['amazon', 'caltech', 'dslr', 'webcam']
-Xs, ys, Xt, yt = readData(domains[0], domains[1])
-
-X = Xs
-y = ys
+test_size = 0.2
+X,X_test,y,y_test = readData(test_size)
 
 learning_rate = 0.1
 n_features = X.shape[1]
@@ -37,27 +36,34 @@ t = 0
 maxt = 1000
 last_loss = 9999
 
+
 while True:
-    scores = np.dot(X, W) + b
-    exp_scores = np.exp(scores)
-    probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
-    correct_logprobs = -np.log(probs[np.arange(n_samples), y])
-    loss = np.sum(correct_logprobs) / n_samples
-    dscores = probs.copy()
-    dscores[np.arange(n_samples), y] -= 1
-    dscores /= n_samples
-    dW = X.T.dot(dscores)
-    db = np.sum(dscores, axis=0, keepdims=True)
-    W = W - learning_rate * dW
-    b = b - learning_rate * db
-    print(loss)
-    if abs(loss - last_loss) < 1e-3:
-        break
-    last_loss = loss
+    loss = 0
+    for index in range(n_samples):
+        scores = np.dot(X, W) + b
+        exp_scores = np.exp(scores)
+        probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+        correct_logprobs = -np.log(probs[np.arange(n_samples), y])
+        loss = np.sum(correct_logprobs) / n_samples
+        dscores = probs.copy()
+        dscores[np.arange(n_samples), y] -= 1
+        dscores /= n_samples
+        dW = X.T.dot(dscores)
+        db = np.sum(dscores, axis=0, keepdims=True)
+        W = W - learning_rate * dW
+        b = b - learning_rate * db
+        print(loss)
+        if abs(loss - last_loss) < 1e-3:
+            break
+        last_loss = loss
 
     t += 1
     if t > maxt:
         break
 
+y_test_socre  = np.dot(X_test,W) + b
+y_test_exp_scores = np.exp(y_test_socre)
+y_test_probs = y_test_exp_scores / np.sum(y_test_exp_scores, axis=1, keepdims=True)
+y_test_probs_max = np.argmax(y_test_probs, axis=1)
 
-
+print(classification_report(y_test, y_test_probs_max))
